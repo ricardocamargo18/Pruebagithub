e60b9db2400d242660c92491700498f9f62bef4e
rcamargol <rcamargol@unbosque.edu.co>
2018-09-16 18:52:19 -0500

    Commit inicial

---
+++ GuiTest/src/FramePpal.java
@@ -0,0 +1,45 @@
+import java.awt.BorderLayout;
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.BoxLayout;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import java.awt.GridLayout;
+import javax.swing.JTextField;
+
+public class FramePpal extends JFrame {
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					FramePpal frame = new FramePpal();
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public FramePpal() {
+		setTitle("Prueba App.Grafica");
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 450, 300);
+		getContentPane().setLayout(null);
+		getContentPane().setLayout(new GridLayout(1, 0, 0, 0));
+	}
+}
---
+++ GuiTest/src/Ventana.java
@@ -0,0 +1,12 @@
+import javax.swing.JPanel;
+
+public class Ventana extends JPanel {
+
+	/**
+	 * Create the panel.
+	 */
+	public Ventana() {
+
+	}
+
+}
